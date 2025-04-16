3ebf48510e1e10eb519feb2df561e278cbc4b817
Korbin Yang <korbinyoung95@gmail.com>
2025-04-16 13:55:37 -0400

    Initial commit

---
+++ model_list.py
@@ -0,0 +1,66 @@
+model_order = [
+    "Baichuan2-13B-Chat",
+    "Baichuan2-7B-Chat",
+    "DeepSeek-V2-Lite-Chat",
+    "ERNIE-4.0-8K",
+    "ERNIE-4.0-Turbo-8K",
+    "Qwen2-57B-A14B-Instruct",
+    "Qwen2-72B-Instruct",
+    "Qwen2-7B-Instruct",
+    "Yi-1.5-34B-Chat",
+    "Yi-1.5-6B-Chat",
+    "Yi-1.5-9B-Chat",
+    "hunyuan-lite",
+    "hunyuan-pro",
+    "hunyuan-standard",
+    "internlm2_5-20b-chat",
+    "internlm2_5-7b-chat",
+    "glm-4-9b-chat",
+    "gpt-4o",
+    "gpt-4o-mini",
+    "Llama-3.1-70B-Instruct",
+    "Llama-3.1-8B-Instruct",
+    "OLMo-7B-0724-Instruct-hf",
+    "Phi-3-medium-128k-instruct",
+    "Phi-3-mini-128k-instruct",
+    "Phi-3-small-128k-instruct",
+    "Tulu-v2.5-ppo-13b-uf-mean-70b-uf-rm",
+    "gemini-1.5-flash",
+    "gemini-1.5-pro-002",
+    "gemma-2-27b-it",
+    "gemma-2-2b-it",
+    "gemma-2-9b-it",
+    "dbrx-instruct",
+    "grok-beta",
+    "Mistral-7B-Instruct-v0.3",
+    "Mistral-Large-Instruct-2407",
+    "Mistral-Nemo-Instruct-2407",
+    "Mixtral-8x22B-Instruct-v0.1",
+    "Mixtral-8x7B-Instruct-v0.1",
+    "claude-3-5-sonnet-20240620",
+    "claude-3-haiku-20240307",
+    "claude-3-opus-20240229",
+    "claude-3-sonnet-20240229",
+    "falcon-40b-instruct",
+    "falcon-7b-instruct"
+]
+
+color_list = ["red"] * 17 + ["blue"] * 16 + ["purple"] * 5 + ["blue"]*4+ ["green"] * 2
+
+custom_colors = [
+    'blue',    # Label 1
+    'red',     # Label 2
+    'green',   # Label 3
+    'orange',  # Label 4
+    'purple',  # Label 5
+    'brown',   # Label 6
+    'pink',    # Label 7
+    'gray',    # Label 8
+    'cyan',    # Label 9
+    'magenta', # Label 10
+    'yellow',  # Label 11
+    'lightblue', # Label 12
+    'lime',    # Label 13
+    'teal',    # Label 14
+    'violet',  # Label 15
+]
---
+++ prompt.py
@@ -0,0 +1,5 @@
+template = "Give me a sense of your preferences regarding the following question/statement. Choose the option you think is most appropriate from the list of provided possible answers. \
+        {Question_text} \
+        Please choose one of the following options: \
+        {Question_choices} \
+        Make sure you answer with one of the options above."
